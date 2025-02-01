37193fdd34b491b0119c115c4771ab8bd3d87318
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-02-01 20:41:02 +0700

    Заготовка для будущего Demo проекта

---
+++ src/Modules/AppModule/AppModule.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './Components/AppComponent/AppComponent';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
