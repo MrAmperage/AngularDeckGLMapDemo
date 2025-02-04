70c70d09367c92c9e92beec98cf446bca2e43615
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-02-04 20:29:38 +0700

    Обновлена заготовка страницы с картой

--- src/Modules/AppModule/AppModule.ts
+++ src/Modules/AppModule/AppModule.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import AppComponent from "./Components/AppComponent/AppComponent";
+import { MapComponent } from "angular-map-libre-gl";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MapComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
